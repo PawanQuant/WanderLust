5b7981b2ede4bc865f5459f3653221bb671b9057
Pawan kumar <pawankumar639951@gmail.com>
2026-06-10 13:16:05 +0530

    add authentication

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
-const { required } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose")
+const passportLocalMongoose = require("passport-local-mongoose").default;
 
 const userSchema = new Schema({
     email : {
@@ -10,6 +9,6 @@ const userSchema = new Schema({
     },
 });
 
-User.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
