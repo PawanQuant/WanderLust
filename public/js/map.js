69bffc714c1bf9ac3a878e2c7c9048ed0e784e61
Pawan kumar <pawankumar639951@gmail.com>
2026-06-19 14:48:05 +0530

    integrate interactive map feature for listing locations

---
+++ public/js/map.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
+    projection: 'globe', // display the map as a globe
+    zoom: 10, // initial zoom level, 0 is the world view, higher values zoom in
+    center: [78.0167 , 27.1833] // center the map on this longitude and latitude
+});
