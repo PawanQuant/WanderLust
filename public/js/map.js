0d57900d3d2dcc1eaaba8c1a0fec7b486d248f78
Pawan kumar <pawankumar639951@gmail.com>
2026-07-27 16:27:59 +0530

    add category filtering

--- public/js/map.js
+++ public/js/map.js
@@ -10,7 +10,7 @@ const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/satellite-streets-v12', // Use the standard style for the map
     projection: 'globe', // display the map as a globe
-    zoom: 10, // initial zoom level, 0 is the world view, higher values zoom in
+    zoom: 12, // initial zoom level, 0 is the world view, higher values zoom in
     center: listing.geometry.coordinates // center the map on this longitude and latitude
 });
 
